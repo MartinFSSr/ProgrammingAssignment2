c0e12d3a21d1f94a1ce51d807e1d82ac4513268d
MartinFSSr <martin.smith@acm.org>
2014-08-23 20:02:29 -0400

    Revised cachematrix.R for Prog Assgn#2
    Coursera Data Science R Programming Assignment#2. Revision of shell
    cachematrix.R file, with makeCacheMatrix and cacheSolve functions.
    (cacheSolve will throw error until x$set() is run.)

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Makes a list of functions to get and set input matrix and 
+## cached matrix inverse.
+## Functions called by cacheSolve() function
 
 makeCacheMatrix <- function(x = matrix()) {
+    inv <- NULL
+    set <- function(yval) {
+      xval <<- yval
+      inv <<- NULL
+    }
+    get <- function() xval
+    setinv <- function(xval) inv <<- solve(xval)
+    getinv <- function() inv
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
+  }
 
-}
-
-
-## Write a short comment describing this function
-
+## Return a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached inverse")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
 }
