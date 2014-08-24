9c9482ce594be947c8fc628443373b08724c1251
MartinFSSr <martin.smith@acm.org>
2014-08-24 12:19:12 -0400

    Second commit of cachematrix.R for R Prog assign#2
    Second commit of R Programming Assignment 2 code.
    Updated with more complete comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,18 @@
-## Makes a list of functions to get and set input matrix and 
+## makeCacheMatrix makes a list of functions to get and set input matrix and 
 ## cached matrix inverse.
-## Functions called by cacheSolve() function
+##
+## Input matrix ASSUMED to be invertable, per instructions (so no checks in code)
+## makeCacheMatrix() functions called by cacheSolve() function
+##
+## NOTE: Instructions do not call for loading the input matrix into cache, so 
+##  program cacheSolve() throws ERROR unless x$set() function is executed
+##  AFTER run of makeCacheMatrix() and BEFORE cacheSolve() is run.
+##  Command like to do this: myfun$set(input_matrix),
+##  assuming prior run of: myfun <- makeCacheMatrix(input_matrix).
+##
+## Usage: 
+##  input_matrix <- matrix(c(5.5, 15:22), ncol=3) (an invertable 3x3 matrix)
+##  myfun <- makeCacheMatrix(input_matrix)   ##creates list of cache functions like myfun$set
 
 makeCacheMatrix <- function(x = matrix()) {
     inv <- NULL
@@ -16,15 +28,17 @@ makeCacheMatrix <- function(x = matrix()) {
          getinv = getinv)
   }
 
-## Return a matrix that is the inverse of 'x'
+## cacheSolve returns cached inverse of input matrix, or calculates one if none exists
+## Usage: cacheSolve(myfun)
+
 cacheSolve <- function(x, ...) {
   
   inv <- x$getinv()
   if(!is.null(inv)) {
-    message("getting cached inverse")
+    message("returning cached inverse")
     return(inv)
   }
-  data <- x$get()
+  data <- x$get()  ## NOTE: Throws error if x$set() has not been previously run
   inv <- solve(data, ...)
   x$setinv(inv)
   inv
